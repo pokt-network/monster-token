582ecaf8dd28b560cd1a4ffa6c5b72578cf00a73
Luis de Leon <luyzdeleon@gmail.com>
2019-01-30 11:48:56 -0400

    Added MonsterToken contract and it's dependencies

--- titanrc.js
+++ titanrc.js
@@ -1,6 +1,30 @@
-module.exports = {
-    "host": "http://127.0.0.1",
-    "port": 8545,
-    "defaultAccount": "",
-    "password": "PLAT4life"
+let nodesmithApiKey = process.env.NODESMITH_API_KEY;
+if (!nodesmithApiKey) {
+    throw new Error("NODESMITH_API_KEY is not valid! Check your titanrc.js file!");
 }
+
+module.exports = {
+    defaultBlockchain: "aion",
+    blockchains: {
+        aion: {
+            networks: {
+                mainnet: {
+                    host: "https://api.nodesmith.io/v1/aion/mainnet/jsonrpc?apiKey=" + nodesmithApiKey,
+                    defaultAccount: "",
+                    password: ""
+                },
+                testnet: {
+                    host: "https://api.nodesmith.io/v1/aion/testnet/jsonrpc?apiKey=" + nodesmithApiKey,
+                    defaultAccount: "",
+                    password: ""
+                },
+                development: {
+                    host: "https://api.nodesmith.io/v1/aion/testnet/jsonrpc?apiKey=" + nodesmithApiKey,
+                    defaultAccount: process.env.DEFAULT_AION_ACCOUNT,
+                    password: "",
+                    privateKey: process.env.DEFAULT_AION_PK
+                }
+            }
+        }
+    }
+};
